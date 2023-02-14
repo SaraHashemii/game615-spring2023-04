102f698470c2aefb6112512e95899be39301fa06
Sara Bayat Hashemi <s.bayathashemi@yahoo.com>
2023-02-14 17:35:52 -0500

    added scripts

--- exercise04/Assets/Scripts/Rotate.cs
+++ exercise04/Assets/Scripts/Rotate.cs
@@ -7,7 +7,7 @@ public class Rotate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(gameObject.name);
+        //Debug.Log(gameObject.name);
     }
 
     // Update is called once per frame
